c4b82a6d7034f1aa67ccce4dcdc49c32a3b6817c
xiayesuifeng <gdigrgg@gmail.com>
2020-02-09 13:54:03 +0800

    add i18n basic implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitlab.com/xiayesuifeng/go-i18n
+
+go 1.13
---
+++ i18n.go
@@ -0,0 +1,33 @@
+package i18n
+
+import (
+	"os"
+	"strings"
+)
+
+var (
+	translations = map[string]Translation{}
+	language     = "en_US"
+)
+
+type Translation map[string]string
+
+func AddTranslation(translation Translation,language string) {
+	translations[language] = translation
+}
+
+func LoadTranslator() {
+	lang := os.Getenv("LANG")
+	if lang != "" {
+		language = lang[:strings.Index(lang,".")]
+	}
+}
+
+func Tr(key string) string {
+	value,ok := translations[language][key]
+	if ok {
+		return value
+	} else {
+		return key
+	}
+}
