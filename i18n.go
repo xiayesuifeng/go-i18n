37dfbe2d0b3c35d1ba093a212539d036ef6e7030
xiayesuifeng <gdigrgg@gmail.com>
2020-02-09 13:57:50 +0800

    add set language function

--- i18n.go
+++ i18n.go
@@ -12,19 +12,23 @@ var (
 
 type Translation map[string]string
 
-func AddTranslation(translation Translation,language string) {
+func AddTranslation(translation Translation, language string) {
 	translations[language] = translation
 }
 
 func LoadTranslator() {
 	lang := os.Getenv("LANG")
 	if lang != "" {
-		language = lang[:strings.Index(lang,".")]
+		language = lang[:strings.Index(lang, ".")]
 	}
 }
 
+func SetLanguage(lang string) {
+	language = lang
+}
+
 func Tr(key string) string {
-	value,ok := translations[language][key]
+	value, ok := translations[language][key]
 	if ok {
 		return value
 	} else {
